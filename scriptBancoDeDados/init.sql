ba220ac07ec9999e4d4727740fe1e36198cd3e2e
Gustavo Reche <gustavodreche@msn.com>
2024-09-03 23:21:04 -0300

    Cria projeto

---
+++ scriptBancoDeDados/init.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE tech_challenge_5_pagamentos;
+
+\c tech_challenge_5_pagamentos;
---
+++ src/main/java/com/fiap/techchallenge5/Techchallenge5Application.java
@@ -0,0 +1,13 @@
+package com.fiap.techchallenge5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Techchallenge5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Techchallenge5Application.class, args);
+	}
+
+}
